cfcf3498585d065ebcadc23ee6830ed5743a8492
David Albert <davidbalbert@gmail.com>
2013-04-11 10:54:35 -0400

    It animates, but I think it's wrong

--- life.rb
+++ life.rb
@@ -1,25 +1,26 @@
 class Board
   attr_reader :size
 
-  def initialize(size)
-    @size = size
-    @board = Array.new(size) do |i|
-      Array.new(size) { |i| rand(2) }
+  def self.random(size)
+    board = Array.new(size) do
+      Array.new(size) { rand(2) }
     end
+    new(board)
+  end
+
+  def initialize(board)
+    @size = board.size
+    @board = board
   end
 
   def next
-    new_board = Board.new(size)
-    each_coordinate do |row, col|
-    end
-  end
-
-  def each_coordinate
-    @board.each_index do |row|
-      @board[row].each_index do |col|
-        yield(row, col)
+    board_state = neighbor_counts.map do |row|
+      row.map do |i|
+        i == 3 || i == 4 ? 1 : 0
       end
     end
+
+    Board.new(board_state)
   end
 
   def to_s
@@ -30,11 +31,37 @@ class Board
     end.join("\n")
   end
 
-  private
-  def []=(row, col, val)
-    @board[row][col] = val
+  def neighbor_counts
+    (-1..1).map { |i| (-1..1).map { |j| [i, j] } }.flatten(1).map do |row, col|
+      rotate_board(row, col)
+    end.reduce(Array.new(@size) { Array.new(@size) { 0 } }) do |res, arr|
+      each_coordinate do |row, col|
+        res[row][col] += arr[row][col]
+      end
+
+      res
+    end
+  end
+
+  def rotate_board(row_count, col_count)
+    @board.map do |row|
+      row.rotate(row_count)
+    end.rotate(col_count)
+  end
+
+  def each_coordinate
+    (0...size).each do |row|
+      (0...size).each do |col|
+        yield row, col
+      end
+    end
   end
 end
 
-b = Board.new(10)
-puts b
+b = Board.random(10)
+loop do
+  b = b.next
+  puts b
+  sleep 0.5
+  system("clear")
+end
